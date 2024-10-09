e7fc82937947370e928ffc8243089b9ca04ba644
sonson-arch <sondrhso@stud.ntnu.no>
2024-10-09 22:10:33 +0200

    #

--- client/src/whiteboard-component.tsx
+++ client/src/whiteboard-component.tsx
@@ -2,54 +2,72 @@ import * as React from 'react';
 import { Component } from 'react-simplified';
 import { Alert } from './widgets';
 
-type Message = { line: { from: { x: number; y: number }; to: { x: number; y: number } } };
+type Message = { user: string; text: string };
 
 export class Whiteboard extends Component {
-  canvas: HTMLCanvasElement | null = null;
-  lastPos: { x: number; y: number } | null = null;
   connection: WebSocket | null = null;
   connected = false;
+  state = {
+    messages: [] as Message[],
+    currentMessage: '',
+    user: 'User' + Math.floor(Math.random() * 1000),
+    users: [] as string[],
+  };
 
   render() {
     return (
       <>
-        <canvas
-          ref={(e) => (this.canvas = e) /* Store canvas element */}
-          onMouseMove={(event) => {
-            // Send lines to Whiteboard server
-            const pos = { x: event.clientX, y: event.clientY };
-            if (this.lastPos && this.connected) {
-              this.connection?.send(JSON.stringify({ line: { from: this.lastPos, to: pos } }));
-            }
-            this.lastPos = pos;
-          }}
-          width={400}
-          height={400}
-          style={{ border: '2px solid black' }}
-        />
-        <div>{this.connected ? 'Connected' : 'Not connected'}</div>
+        <div>
+          <h1>Connected users:</h1>
+          <ul>
+            {this.state.users.map((user, index) => (
+              <li key={index}>{user}</li>
+            ))}
+          </ul>
+
+          <h1>Messages:</h1>
+          <div>
+            {this.state.messages.map((message, index) => (
+              <div key={index}>
+                <strong>{message.user}:</strong> {message.text}
+              </div>
+            ))}
+          </div>
+          <input
+            type="text"
+            value={this.state.currentMessage}
+            onChange={(event) => this.setState({ currentMessage: event.target.value })}
+          />
+          <button onClick={this.sendMessage}>Send</button>
+          <div>{this.connected ? 'Connected' : 'Not connected'}</div>
+        </div>
       </>
     );
   }
 
   mounted() {
     // Connect to the websocket server
-    this.connection = new WebSocket('ws://localhost:3000/api/v1/whiteboard');
+    this.connection = new WebSocket('ws://localhost:3000');
 
     // Called when the connection is ready
     this.connection.onopen = () => {
       this.connected = true;
+      // Send the current user to the server
+      this.connection?.send(JSON.stringify({ user: this.state.user }));
     };
 
     // Called on incoming message
     this.connection.onmessage = (message) => {
-      const context = this.canvas?.getContext('2d');
-      context?.beginPath();
-      const data: Message = JSON.parse(message.data);
-      context?.moveTo(data.line.from.x, data.line.from.y);
-      context?.lineTo(data.line.to.x, data.line.to.y);
-      context?.closePath();
-      context?.stroke();
+      const data = JSON.parse(message.data);
+      if (data.text) {
+        // It's a message
+        this.setState((prevState) => ({
+          messages: [...prevState.messages, data],
+        }));
+      } else if (data.users) {
+        // It's the updated list of users
+        this.setState({ users: data.users });
+      }
     };
 
     // Called if connection is closed
@@ -64,6 +82,17 @@ export class Whiteboard extends Component {
       Alert.danger('Connection error');
     };
   }
+
+  sendMessage = () => {
+    if (this.connection && this.connected) {
+      const message = {
+        user: this.state.user,
+        text: this.state.currentMessage,
+      };
+      this.connection.send(JSON.stringify(message));
+      this.setState({ currentMessage: '' }); // Clear the input field after sending
+    }
+  };
 
   // Close websocket connection when component is no longer in use
   beforeUnmount() {
